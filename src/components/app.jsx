8e434f7dc963c50859b689828df4307541cbcddd
SDhakZ <sharnam34@gmail.com>
2022-08-09 20:37:36 +0545

    first commit

---
+++ src/components/app.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Contact from "./contact";
+
+function App(){
+    return(
+        <div>
+            <Contact/>
+        </div>
+    );
+}
+export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useState, useEffect} from "react";
+
+function Counter(){
+    return(
+        <h1>Data</h1>
+    );
+}
+export default Counter;
